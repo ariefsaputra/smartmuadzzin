22d0cc69533ede884d4c7bccfa92101e1ee1aadf
Arief Saputra <arief.isaputra@gmail.com>
2026-09-12 10:58:01 +0700

    fixing durasi waktu sholat

--- app/Views/tv/layout.php
+++ app/Views/tv/layout.php
@@ -238,12 +238,19 @@ function tvDisplay() {
                     maghrib: <?= (int) ($pengaturan['durasi_iqamah_maghrib'] ?? $pengaturan['durasi_menjelang_iqamah'] ?? 300) ?>,
                     isya: <?= (int) ($pengaturan['durasi_iqamah_isya'] ?? $pengaturan['durasi_menjelang_iqamah'] ?? 300) ?>
                 },
-                prayer: <?= (int) ($pengaturan['durasi_waktu_sholat'] ?? 600) ?>,
+                prayer: {
+                    subuh: <?= (int) ($pengaturan['durasi_waktu_sholat_subuh'] ?? $pengaturan['durasi_waktu_sholat'] ?? 600) ?>,
+                    dzuhur: <?= (int) ($pengaturan['durasi_waktu_sholat_dzuhur'] ?? $pengaturan['durasi_waktu_sholat'] ?? 600) ?>,
+                    ashar: <?= (int) ($pengaturan['durasi_waktu_sholat_ashar'] ?? $pengaturan['durasi_waktu_sholat'] ?? 600) ?>,
+                    maghrib: <?= (int) ($pengaturan['durasi_waktu_sholat_maghrib'] ?? $pengaturan['durasi_waktu_sholat'] ?? 600) ?>,
+                    isya: <?= (int) ($pengaturan['durasi_waktu_sholat_isya'] ?? $pengaturan['durasi_waktu_sholat'] ?? 600) ?>
+                },
                 khutbahJumat: <?= (int) ($pengaturan['durasi_khutbah_jumat'] ?? 1200) ?>
             };
             let matched = false;
             if (now.getDay() === 5) {
                 const dzuhurTime = this.prayerTimes.dzuhur;
+                const jumatPrayerDuration = durations.prayer.dzuhur;
                 if (dzuhurTime && dzuhurTime !== '--:--') {
                     const diff = (new Date(`${now.toDateString()} ${dzuhurTime}`) - now) / 1000;
                     if (diff > 0 && diff <= durations.pre) {
@@ -255,7 +262,7 @@ function tvDisplay() {
                     } else if (diff <= -durations.adzan && diff > -(durations.adzan + durations.khutbahJumat)) {
                         this.setOverlay('jumat_khutbah', 'JUMAT', '');
                         matched = true;
-                    } else if (diff <= -(durations.adzan + durations.khutbahJumat) && diff > -(durations.adzan + durations.khutbahJumat + durations.prayer)) {
+                    } else if (diff <= -(durations.adzan + durations.khutbahJumat) && diff > -(durations.adzan + durations.khutbahJumat + jumatPrayerDuration)) {
                         this.setOverlay('jumat_sholat', 'JUMAT', '');
                         matched = true;
                     }
@@ -267,7 +274,8 @@ function tvDisplay() {
                     const time = this.prayerTimes[name];
                     if (!time || time === '--:--') continue;
                     const diff = (new Date(`${now.toDateString()} ${time}`) - now) / 1000;
-                    const iqamahDuration = durations.iqamah[name] * 60;
+                    const iqamahDuration = durations.iqamah[name];
+                    const prayerDuration = durations.prayer[name];
                     if (diff > 0 && diff <= durations.pre) {
                         this.setOverlay('menjelang_adzan', name.toUpperCase(), this.countdown(diff));
                         matched = true;
@@ -283,7 +291,7 @@ function tvDisplay() {
                         matched = true;
                         break;
                     }
-                    if (diff <= -(durations.adzan + iqamahDuration) && diff > -(durations.adzan + iqamahDuration + durations.prayer)) {
+                    if (diff <= -(durations.adzan + iqamahDuration) && diff > -(durations.adzan + iqamahDuration + prayerDuration)) {
                         this.setOverlay('waktu_sholat', name.toUpperCase(), '');
                         matched = true;
                         break;
